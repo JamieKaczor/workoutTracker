939b80d4bfa661b4f84fac9be958465e2875c817
Jamie Kaczor <dignanjk@aol.com>
2021-07-08 14:41:47 -0400

    deploying

--- server.js
+++ server.js
@@ -13,12 +13,13 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/workout', {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    });
 
 // routes
 app.use(require('./routes/api.js'));
